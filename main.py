d897fabd8cefa76512983d9b37d19e1cd17f14cd
Jordi <jordi.marin@enti.cat>
2025-01-02 14:15:46 +0100

    Icono y fuente
    He añadido el icono para el boton de encriptar y la fuente para el texto, no es la fuente definitiva.

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ def seleccionar_archivo():
 
 def leer_archivo():
     """Funcion para leer el archivo"""
-        
+    pass  # Implementar en el futuro
+
 # Crear ventana principal
 ventana = ctk.CTk()
 ventana.title("Selector de Archivos")
@@ -27,20 +28,30 @@ ventana.geometry("500x200")
 frame = ctk.CTkFrame(ventana, width=300, height=100, fg_color='transparent')
 frame.pack(expand=True)
 
+# Cargar la fuente personalizada
+fuente_personalizada = ctk.CTkFont(family= 'Coolvetica', size= 20)
+
 # Etiqueta para mostrar el archivo seleccionado
 etiqueta_archivo = ctk.CTkLabel(
-    frame, text="No se ha seleccionado ningún archivo.", wraplength=450, justify="left"
+    frame, text="No se ha seleccionado ningún archivo.", wraplength=450, justify="left", font= fuente_personalizada
 )
 etiqueta_archivo.pack(pady=20)
 
 # Cargar ícono de archivo
 try:
     foto = Image.open('File_Icon.png')
-    icono_imagen = ctk.CTkImage(light_image=foto, dark_image=foto, size=(32, 32))
+    icono_imagen = ctk.CTkImage(light_image=foto, dark_image=foto, size=(44, 44))
 except FileNotFoundError:
     print("Error: File_Icon.png no encontrado. El botón se mostrará sin imagen.")
     icono_imagen = None
-
+    
+try:
+    foto2 = Image.open('Encrypt_Icon.png')
+    encriptar_imagen = ctk.CTkImage(light_image= foto2, dark_image= foto2, size=(44,44))
+except FileNotFoundError:
+    print("Error: Encrypt_Icon.png no encontrado. El botón se mostrará sin imagen.")
+    encriptar_imagen = None
+    
 # Botón para subir archivos
 boton_subir = ctk.CTkButton(
     frame,
@@ -50,6 +61,7 @@ boton_subir = ctk.CTkButton(
     border_color='#FF5733',
     border_width=2,
     hover_color='#362522',
+    font= fuente_personalizada,
     image=icono_imagen,
     command=seleccionar_archivo
 )
@@ -64,6 +76,8 @@ button = ctk.CTkButton(
     border_color='#FF5733',
     border_width=2,
     hover_color='#362522',
+    font= fuente_personalizada,
+    image= encriptar_imagen,
     command=leer_archivo)
 button.pack(padx=10, pady=10, side='right')
 
